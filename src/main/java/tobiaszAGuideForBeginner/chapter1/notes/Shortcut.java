9b4a194b4959586548a862e2982df6ae6e646763
tobiasz b <tobiaszbieda@gmail.com>
2019-02-04 21:21:10 +0100

    [tobiasz][notes] I Create class Shortcut and add Shortcut

--- src/main/java/tobiaszAGuideForBeginner/chapter1/notes/Shortcut.java
+++ src/main/java/tobiaszAGuideForBeginner/chapter1/notes/Shortcut.java
@@ -2,9 +2,53 @@ package tobiaszAGuideForBeginner.chapter1.notes;
 
 public class Shortcut {
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Debugging
 
-/*
+    Step over               -- F8 --            (krok na przod w obrebie metody)
+    Step into               -- F7 --            (krok na przod w obrebie metody ale przechodzi do innych metod jesli sa dodane)
+    Run to cursor           -- Shift + F9 --    ( wlacz debaga)
+    Resume program          -- F9 --            (kolejny breakepoint)
 
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Search/Replace
 
+    Find                    -- Ctrl + F --      (szukanie w obrebie klasy)
+    Find in Path            -- Ctrl + Shift + F -- (szukanie we wszystkich klasach)
+    Find everywhere         -- Double Shift --
+
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Editing
+
+    Basic code completion   -- Ctrl + space --  (podstawowe uzupelnianie kodu)
+    Generate code           -- Alt + Insert --  (generowanie kodu) pozwala wygenerować zarówno gettery, settery, konstruktory jak i metody z klasy Object jak equals(), czy toString()
+    Extend selection        -- Ctrl + W --      (zaznaczanie kodu po linijce)
+    Show intention          -- Alt + Enter --   (w zależności od kontekstu pokazuje różne możliwości)
+    actions and quick fixes
+    Duplicate current line  -- Ctrl + D
+    Reformat code           -- Ctrl + Alt + L -- (formatowanie kodu)
+    Moving the selected     -- Ctrl + Shift + Top/Down ( przesowanie strzalkami zaznaczonego kodu)
+    code
+
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Refactoring
+
+    Rename                  -- Shift + F6
+    Extract Variable        -- Ctrl + Alt + V -- (wyciaga zmienne)
+
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* Navigate
+
+    Navigate                -- Ctrl + LeftMouse -- (sprawdzanie sciezki)
+
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* UnitTest
+
+    Create test             -- Ctrl + Shift + T
 
  */
